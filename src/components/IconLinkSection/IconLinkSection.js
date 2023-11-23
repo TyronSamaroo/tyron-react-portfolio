0a805c4643d2fac0a70cfd1592b0cf5505bb638b
Tyron Samaroo <tyronsamaroo828@gmail.com>
2023-11-22 20:27:25 -0500

    Update code to decouple Skill Section to use reusable component IconLinkSection

--- src/components/IconLinkSection/IconLinkSection.js
+++ src/components/IconLinkSection/IconLinkSection.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { DiGithub, DiLinkedin} from 'react-icons/di';
+import { DiGithub, DiLinkedin, DiPython} from 'react-icons/di';
 import { motion } from 'framer-motion';
 import './IconLinkSection.css';
 
-const IconLinkSection = ( { links, heading }) => (
+const IconLinkSection = ( { title, items }) => (
     <div className='icon-link-container'>
-        <h2>{heading}</h2>
+        <h2>{title}</h2>
         <div className='icon-link-grid'>
-            {links.map((link, index) => (
-                <a href={link.url} key={index} target='_blank' rel='noopener noreferrer'>
+            {items.map((item, index) => (
+                <a href={item.url} key={index} target='_blank' rel='noopener noreferrer'>
                     <div className='icon-link'>
                         <motion.div whileHover={{ scale: 1.1}}>
-                            <link.icon className='icon'/>
+                            <item.icon className='icon'/>
                         </motion.div>
-                        <h3>{link.name}</h3>
+                        <h3>{item.name}</h3>
                     </div>
                 </a>
             ))}
--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -1,43 +1,15 @@
 import React from 'react';
-import { DiPython, DiJavascript1, DiReact, DiJava, DiAws } from 'react-icons/di';
+import { DiPython, DiJavascript1, DiReact, DiJava, DiAws, DiJavascript } from 'react-icons/di';
 import { motion } from 'framer-motion';
 import './SkillsSection.css';
+import IconLinkSection from '../IconLinkSection/IconLinkSection';
 
-const SkillsSection = () => (
-    <div className="skills-container">
-      <h2>Technical Skills</h2>
-      <div className="skills-grid">
-        <div className="skill">
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <DiPython className="skill-icon" />
-          </motion.div>
-          <h3>Python</h3>
-        </div>
-        <div className="skill">
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <DiJavascript1 className="skill-icon" />
-          </motion.div>
-          <h3>JavaScript</h3>
-        </div>
-        <div className="skill">
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <DiReact className="skill-icon" />
-          </motion.div>
-          <h3>React</h3>
-        </div>
-        <div className="skill">
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <DiJava className="skill-icon" />
-          </motion.div>
-          <h3>Java</h3>
-        </div>
-        <div className="skill">
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <DiAws className="skill-icon" />
-          </motion.div>
-          <h3>AWS</h3>
-        </div>
-      </div>
-    </div>
-  );
+const SkillsSection = () => {
+  const skills = [
+    { icon: DiPython, label:'Python', url: 'https://www.python.org/' },
+    { icon: DiJavascript, label:'JavaScript', url: 'https://www.python.org/' },
+  ]
+
+  return <IconLinkSection title="Technical Skill Section" items={skills}/>;
+}
 export default SkillsSection;
